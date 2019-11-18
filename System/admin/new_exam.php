afa2783c8148bcd252debee67a0abaf8df8c7487
Thanh Phong <ntphong1999qnvn@gmail.com>
2019-11-18 21:20:59 +0700

    ntpac

--- System/admin/new_exam.php
+++ System/admin/new_exam.php
@@ -12,13 +12,15 @@
 include '../db_config/connection.php';
 
 $sqlString = "SELECT * FROM exam";
-if ($result = $conn->query($sqlString)) 
-	{
-		while ($row = $result->fetch_array()) 
-		{
-			$dem++;
+$result = $conn->query($sqlString);
+	while ($row = $result->fetch_array()) 
+		{	if($row['question'] == $ques)
+			{
+				header("location:new_examfontend.php?err= cau hoi da ton tai");
+			}
+			else
+				$dem++;
 		}
-	}
 	$dem++;
 	if($dem<10)
 	{
@@ -35,16 +37,14 @@ if ($result = $conn->query($sqlString))
 $sql = "INSERT INTO exam (question_id, sub, question, option1, option2, option3,option4,answer)
 VALUES ('$dem', N'$sub', N'$ques', N'$op1', N'$op2', N'$op3', N'$op4', N'$ans')";
 
-if ($conn->query($sql) === TRUE) {
+if ($conn->query($sql)) {
     header("location:new_examfontend.php?message=$ques have been registered with ID $dem");
 } 
 else {
 	$error = $conn->error;
      header("location:new_examfontend.php?err=$error");
 }
-
 $conn->close();
-
 }else{
 	header("location:./");
 }
--- System/admin/new_tea.php
+++ System/admin/new_tea.php
@@ -9,10 +9,8 @@ $gender = $_POST['gender'];
 
 include '../db_config/connection.php';
 
-$sql = "SELECT * FROM user_info where email = '$teaem'";
+$sql = "SELECT * FROM user_info";
 $result = $conn->query($sql);
-
-if ($result->num_rows > 0) {
 
     while($row = $result->fetch_assoc()) 
 	{
@@ -22,15 +20,14 @@ if ($result->num_rows > 0) {
 		return;
 		}
     }
-} else {
-  $regdate = date('jS \ F Y h:i:s A');
+$regdate = date('jS \ F Y h:i:s A');
 $teano = 'STD:'.rand(1000,9999).'/'.rand(10,99).'/'.rand(0,9).'';
 
 $sql = "INSERT INTO user_info (user_id, full_name, sub, gender, email, address, role, regdate)
-VALUES ('$teano', '$teaname', '$teasub', '$gender','$teaem', '$teaadd','Teacher', '$regdate')";
-}
+VALUES ('$teano', '$teaname', N'$teasub', '$gender','$teaem', '$teaadd','Teacher', '$regdate')";
 
-if ($conn->query($sql) === TRUE) {
+
+if ($conn->query($sql)) {
     header("location:new_teacher.php?message=$teaname have been registered with ID $teano");
 } else {
 	$error = $conn->error;
